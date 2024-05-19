b21d622cba2d1e4b0a94ae3d144804f6802fadbd
Radhakrushna Mahadik <mahdikrs512@gmail.com>
2024-05-19 10:31:37 +0530

    console.log and variables

---
+++ 01_variables.js
@@ -0,0 +1,28 @@
+// to print the variable on the console or any string on the console we have to use the console.log in javascript
+
+console.log("Hello World and welcome to javascript");
+
+console.log("Radhakrushna Mahadik");
+
+console.log("You are here because you deserved!");
+
+// console.log() will always print the code on new line and hence we dont need to add any extra new line character
+
+// alert("Are you okay?");
+
+// variables are used to store the values inside them
+// They will be assigned the memory based on the datatype present there
+
+age = 22; 
+my_name = "Radhakrushna Mahadik";
+score = 96.53;
+// Variables can be declared without using any datatype and hence it can be considered as dynamically typed language
+
+console.log("Hello my age is " + age);
+console.log("Hello my age is" , age);
+// To add the two strings we can use + symbol or we can even use , operator and hence we can use both to print on the console
+// Major difference between + operator or , operator is that + operator will not add any white space and it will just concanate the string to the variable and hence we have to expliciltly add the white space out there
+// But when we use the , operator the white space will be added to the variable no matter what
+
+console.log("My name is", my_name);
+console.log("My score is " + score);
